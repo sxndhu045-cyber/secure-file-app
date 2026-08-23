ef7744b95fe5a32a2a5cfd61aa6036958798fd6d
Your Name <your_email@gmail.com>
2026-08-23 17:09:17 +0530

    Prepare project for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -89,11 +89,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
